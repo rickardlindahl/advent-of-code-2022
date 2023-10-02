f1c5dc8c65d5bc3bd2ca0f571facee722b212332
Rickard Lindahl <rickard@lindahl.app>
2023-10-02 14:58:13 +0200

    Day 1 in rust.

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+mod day01;
+
+fn main() {
+    println!("part1");
+    println!("{}", day01::part1());
+
+    println!();
+    println!();
+    println!();
+
+    println!("part2");
+    println!("{}", day01::part2());
+}

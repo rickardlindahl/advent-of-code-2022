be210d1ba67635ed04bc4170a294fc9a1e885724
Rickard Lindahl <rickard@lindahl.app>
2023-10-03 12:04:41 +0200

    Put main on top of file

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -3,6 +3,16 @@ mod input;
 
 use elf::Elf;
 
+fn main() {
+    let input_file_path = concat!(env!("CARGO_MANIFEST_DIR"), "/src/input.txt");
+    let mut elves: Vec<Elf> = Vec::new();
+    input::get_elves_from_input(input_file_path, &mut elves);
+    println!("---- Day 1 ----");
+    println!("Part 1: {}", part1(&elves));
+    println!("Part 2: {}", part2(&elves));
+    println!("---------------");
+}
+
 pub fn part1(elves: &Vec<Elf>) -> u32 {
     let most_calories = elves.into_iter().fold(0, |acc, elf| {
         if elf.calories > acc {
@@ -30,14 +40,4 @@ pub fn part2(elves: &Vec<Elf>) -> u32 {
     });
 
     first + second + third
-}
-
-fn main() {
-    let input_file_path = concat!(env!("CARGO_MANIFEST_DIR"), "/src/input.txt");
-    let mut elves: Vec<Elf> = Vec::new();
-    input::get_elves_from_input(input_file_path, &mut elves);
-    println!("---- Day 1 ----");
-    println!("Part 1: {}", part1(&elves));
-    println!("Part 2: {}", part2(&elves));
-    println!("---------------");
 }
